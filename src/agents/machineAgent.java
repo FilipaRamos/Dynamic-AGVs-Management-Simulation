a41806c0b9cd709a6510b5c15febd650646883a9
João Baião <joaobaiao12@gmail.com>
2016-12-03 19:40:07 +0000

    machineAgent and spaces initialized

---
+++ src/agents/machineAgent.java
@@ -0,0 +1,136 @@
+package agents;
+
+
+public class machineAgent {
+    private int capacity;
+    private int velocity;
+    private boolean maintenance;
+    private int x, y;
+    private int potential;
+    private int lotsProducing;
+
+    private int processingStep;
+    private int stepID;
+
+    /**
+     * Constructor of a machine agent
+     * @param processID the id of the processing the machine belongs to
+     * @param stepID the id inside the processID
+     * @param cap processing capacity of the machine
+     * @param vel processing velocity of the machine
+     * @param x x coordinate
+     * @param y y coordinate
+     */
+    public machineAgent(int processID, int stepID, int cap, int vel, int x, int y){
+        this.processingStep = processID;
+        this.stepID = stepID;
+        this.capacity = cap;
+        this.velocity = vel;
+        this.maintenance = false;
+        this.x = x;
+        this.y = y;
+        this.lotsProducing = 0;
+        this.potential = (1/cap) * (1/vel);
+    }
+
+    /**
+     * add a lot to the machine
+     * @return if the lot was added
+     */
+    public boolean addLot(){
+        if (capacity > lotsProducing) {
+            lotsProducing++;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    /**
+     * report the state of the machine in console
+     */
+    public void report(){
+        System.out.println(getProcessingStep() +
+                "." + getStepID() + " with capacity of " +
+                capacity + ", has " + lotsProducing +
+                " lots processing with velocity of " +
+                velocity + ", and is at [" +
+                x + "," + y + "]");
+    }
+
+
+
+
+    //GET's e SET's
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+
+    public boolean isMaintenance() {
+        return maintenance;
+    }
+
+    public void setMaintenance(boolean maintenance) {
+        this.maintenance = maintenance;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getPotential() {
+        return potential;
+    }
+
+    public void setPotential(int potential) {
+        this.potential = potential;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getProcessingStep() {
+        return processingStep;
+    }
+
+    public void setProcessingStep(int processingStep) {
+        this.processingStep = processingStep;
+    }
+
+    public int getStepID() {
+        return stepID;
+    }
+
+    public void setStepID(int stepID) {
+        this.stepID = stepID;
+    }
+
+    public int getLotsProducing() {
+        return lotsProducing;
+    }
+
+    public void setLotsProducing(int lotsProducing) {
+        this.lotsProducing = lotsProducing;
+    }
+}
---
+++ src/spaces/space.java
@@ -0,0 +1,72 @@
+package spaces;
+
+import uchicago.src.sim.space.Object2DGrid;
+
+/**
+ * Created by test on 03/12/2016.
+ */
+public class space {
+    private Object2DGrid AGVSpace;
+    private Object2DGrid machineSpace;
+
+    /**
+     * Constructor of AGV and machines spaces
+     * @param xSize width of the space
+     * @param ySize height of the space
+     */
+    public space(int xSize, int ySize){
+        AGVSpace = new Object2DGrid(xSize, ySize);
+        machineSpace = new Object2DGrid(xSize, ySize);
+    }
+
+    /**
+     * move and AGV
+     * @param x x coordinate of AGV current position
+     * @param y y coordinate of AGV current position
+     * @param newX new x coordinate of AGV position
+     * @param newY new y coordinate of AGV position
+     * @return if the AGV can move
+     */
+    public boolean moveAGV(int x, int y, int newX, int newY){
+        boolean retVal = false;
+        if(!isCellOccupied(newX, newY)){
+            AGVAgent agv = (AGVAgent)AGVSpace.getObjectAt(x, y);
+            removeAGV(x, y);
+            agv.move(newX, newY);
+            AGVSpace.putObjectAt(newX, newY, agv);
+            retVal = true;
+        }
+        return retVal;
+    }
+
+    /**
+     * verifies if cell (x, y) is occupied
+     * @param x x coordinate
+     * @param y y coordinate
+     * @return if the cell is occupied
+     */
+    public boolean isCellOccupied(int x, int y){
+        boolean retVal = false;
+        if(AGVSpace.getObjectAt(x, y)!=null)
+            retVal = true;
+        return retVal;
+    }
+
+    /**
+     * remove an AGV from AGVSpace in (x,y)
+     * @param x x coordinate
+     * @param y y coordinate
+     */
+    public void removeAGV(int x, int y){
+        AGVSpace.putObjectAt(x, y, null);
+    }
+
+    public Object2DGrid getCurrentAGVSpace() {
+        return AGVSpace;
+    }
+
+    public Object2DGrid getCurrentMachineSpace() {
+        return machineSpace;
+    }
+
+}
