88cc7fff224a77ff22f6b642f249646870a2813a
FilipaRamos <fimamora@hotmail.com>
2016-12-05 22:49:59 +0000

    fixed mistakes

--- src/agents/MachineAgent.java
+++ src/agents/MachineAgent.java
@@ -313,8 +313,8 @@ public class MachineAgent extends Agent implements Drawable{
                 // Message received. Process it
                 String title = msg.getContent();
                 ACLMessage reply = msg.createReply();
-                Integer price = (Integer) catalogue.get(title);
-                if (price != null) {
+               // Integer price = (Integer) get(title);
+                /*if (price != null) {
                     // The requested book is available for sale. Reply with the price
                     reply.setPerformative(ACLMessage.PROPOSE);
                     reply.setContent(String.valueOf(price.intValue()));
@@ -323,7 +323,7 @@ public class MachineAgent extends Agent implements Drawable{
                     // The requested book is NOT available for sale.
                     reply.setPerformative(ACLMessage.REFUSE);
                     reply.setContent("not-available");
-                }
+                }*/
                 myAgent.send(reply);
             }else{
                 block();
