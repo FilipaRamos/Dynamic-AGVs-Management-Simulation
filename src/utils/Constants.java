67ad502856cf9b1180140d76481a64631d52f969
FilipaRamos <fimamora@hotmail.com>
2016-12-05 22:49:31 +0000

    added more files

---
+++ src/utils/Constants.java
@@ -0,0 +1,12 @@
+package utils;
+
+/**
+ * Created by utilizador on 05/12/2016.
+ */
+public class Constants {
+
+    public static final int PROCESSED = 1;
+    public static final int NEGOTIATED_MACHINE = 2;
+
+
+}
